4bfc8cb0f9342acbdfa46e548d9ecb5e4e01126c
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-17 00:19:42 +0000

    first code template

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-/**
- *Puts function - display a litle quote about programming
- */
-
 #include <stdio.h>
-
+/**
+ *main- entry point
+ *Return: 0 in succes or other number in failure
+ */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
