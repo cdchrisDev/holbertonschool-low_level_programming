263272eecf6f8f4a32b374c31b4bcafb2154b02e
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-18 23:41:23 +0000

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+/**
+ *main - entry point
+ *@void: No takes parameters
+ *Description: This function display the alphabet
+ *Return: 0 = nothing but the putchar() function
+ */
 
 int main(void)
 {
 	int Alph;
+
 	for (Alph = 'a'; Alph <= 'z'; Alph++)
 		putchar(Alph);
-	return 0;
+	putchar('\n');
+
+	return (0);
 }
 
