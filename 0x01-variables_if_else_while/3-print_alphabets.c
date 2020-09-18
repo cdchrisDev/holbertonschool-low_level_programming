a863fa1994d5fa48842f81383f2d80c46cd64575
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-18 23:44:14 +0000

    alphAbet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ *main - entry point
+ *@void: Not takes parameters
+ *Description: This function will display the alphabet
+ *in capital and minus form
+ *Return: nothing but the putchar function
+ */
 
 int main(void)
 {
@@ -9,5 +16,7 @@ int main(void)
 		putchar(alph);
 	for (Alph = 'A'; Alph < 'Z'; Alph++)
 		putchar(Alph);
+	putchar('\n');
+
 	return (0);
 }
