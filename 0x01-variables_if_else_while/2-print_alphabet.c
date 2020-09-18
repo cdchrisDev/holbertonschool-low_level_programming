bc202055fa1791e265333636ba77da9a0c9d53b3
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-18 23:08:52 +0000

    jm

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int Alph;
+	for (Alph = 'a'; Alph <= 'z'; Alph++)
+		putchar(Alph);
+	return 0;
+}
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int alph;
+	int Alph;
+
+	for (alph = 'a'; alph < 'z'; alph++)
+		putchar(alph);
+	for (Alph = 'A'; Alph < 'Z'; Alph++)
+		putchar(Alph);
+	return (0);
+}
