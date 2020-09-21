ad6859f753badf26ea9e8b9e3f1db00f4405d268
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-21 20:42:13 +0000

    Multi _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,33 @@
 #include "holberton.h"
 /**
  *main - entry point
- *@void: No takes parameters
- *Description: This function display the alphabet
- *Return: 0 = nothing but the putchar() function
+ *@void: takes no arguments
+ *Return: 0 = nothing but the _putchar() function
  */
 
 int main(void)
 {
+int Hol = 'H';
 
-char Alph;
+	_putchar(Hol);
+		Hol = 'o';
+	_putchar(Hol);
+		Hol = 'l';
+	_putchar(Hol);
+		Hol = 'b';
+	_putchar(Hol);
+		Hol = 'e';
+	_putchar(Hol);
+		Hol = 'r';
+	_putchar(Hol);
+		Hol = 't';
+	_putchar(Hol);
+		Hol = 'o';
+	_putchar(Hol);
+		Hol = 'n';
+	_putchar(Hol);
 
-	for (Alph = 'a'; Alph <= 'z'; Alph++)
-	{
-		_putchar(Alph);
-	}
-		_putchar('\n');
+	_putchar('\n');
+
 	return (0);
 }
-
