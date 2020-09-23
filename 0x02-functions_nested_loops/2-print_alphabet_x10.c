deff4c5cb2e1c638be5bd748d29dcc26c694da5a
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-23 23:51:10 +0000

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *mian - entry point
+ *@void: takes no arguments
+ *Description: increment letter a until reach z by 10 times
+ *Return: print the alphabet x10
+ */
+
+void print_alphabet_x10(void)
+{
+	int Alph;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (Alph = 'a'; Alph <= 'z'; Alph++)
+		{
+			_putchar(Alph);
+		}
+	_putchar('\n');
+	}
+}
+
