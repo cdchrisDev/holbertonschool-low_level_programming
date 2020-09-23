a867347697f32999baa9500bdb4b773d85b5f8e3
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-23 00:10:43 +0000

    ho

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,27 +7,15 @@
 
 int main(void)
 {
-int Hol = 'H';
+	int _putchar(char c);
 
-	_putchar(Hol);
-		Hol = 'o';
-	_putchar(Hol);
-		Hol = 'l';
-	_putchar(Hol);
-		Hol = 'b';
-	_putchar(Hol);
-		Hol = 'e';
-	_putchar(Hol);
-		Hol = 'r';
-	_putchar(Hol);
-		Hol = 't';
-	_putchar(Hol);
-		Hol = 'o';
-	_putchar(Hol);
-		Hol = 'n';
-	_putchar(Hol);
+	char Hol[] = "Holberton";
+	int letter;
 
+	for (letter = 0; letter <= 8; letter++)
+	{
+		_putchar(Hol[letter]);
+	}
 	_putchar('\n');
-
 	return (0);
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,23 +2,9 @@
 #ifndef _PUTCHAR
 #define _PUTCHAR
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+int char Hol[] = "Holberton";
+int letter;
 
 #endif
-/* start of 1-alphabet.c */
-int print_alphabet(void)
-{
-	int Alph;
 
-	for (Alph = 'a'; Alph <= 'z'; Alph++)
-	{
-		_putchar(Alph);
-	}
-	_putchar('\n');
-
-	return (0);
-}
-/* end of 1-alphabet.c */
