aa066e0601032ba32992e4db4c7b7da5154a148b
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-24 01:56:49 +0000

    trial1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_islower - entry point
+ *@c: lowercase or uppercase char
+ *Description: look for lowercase chars
+ *Return: 1 if c is lowercase, otherwise 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		int c = '1';
+
+		_putchar(c);
+	}
+	else
+	{
+		int c = '0';
+
+		_putchar(c);
+	}
+	return (0);
+}
+
