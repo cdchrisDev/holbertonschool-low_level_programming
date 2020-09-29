60f47ae08aa780cef8ee6c056ab1e70b80fb88f8
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-29 16:30:48 +0000

    trial2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 /**
  *swap_int - entry point
- *@*a, @*b: the swap numbers
+ *@a: the swap number
+ *@b: the swap number
  *Description: this function swap the values of the variables
  *return: it returned in void
  */
 void swap_int(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
+
 	*a = *b;
 	*b = temp;
 }
