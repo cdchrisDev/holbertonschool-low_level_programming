3c4e20f2b2f4e66d1c55e75ed2731f057635dca2
cdchrisDev <juan.feli.lopez@gmail.com>
2020-09-29 19:17:27 +0000

    trial1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_strlen - lenght of the string
+ *@s: lenght of the value
+ *Return: int returned
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	/* array condition in with value i */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
