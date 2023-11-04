e901b6df1394aeefa162b19e665e47f416fa3898
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-04 13:20:47 -0500

    done

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * print numbers 1 to 9
+ * @void: no parameters
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	n = -1;
+
+	while (n <= 15)
+	{
+		putchar('1' + n);
+		n++;
+
+		if (n >= 9)
+		{
+			n = 'a';
+
+			while (n != 'g')
+			{
+				putchar(n);
+				n++;
+			}	
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
