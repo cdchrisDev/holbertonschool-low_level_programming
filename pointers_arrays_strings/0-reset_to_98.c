4a2f8909199f20ea483b6171efd7fc43685019ab
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-07 05:28:53 -0500

    updated

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - prototype func
+ * a function to reset a 'n' to 98
+ * @void: no parameters
+ * Return: 0 
+ */
+void reset_to_98(int *n)
+{
+	n = 98;
+
+	return (0);
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _main_
+#define _main:
+
+void reset_to_98(int *n);
+
+#endfi
