caa2eba74c871ee67ea3e544c4340f212d5f7f66
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-07 05:55:17 -0500

    first try

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,11 @@
 /**
  * reset_to_98 - prototype func
  * a function to reset a 'n' to 98
- * @void: no parameters
- * Return: 0 
+ * @n: number to update
+ * Return: updated number
  */
 void reset_to_98(int *n)
 {
-	n = 98;
+	*n = 98;
 
-	return (0);
 }
