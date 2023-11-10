83933bf5d65b21af2e3f678db7fee93ba205c2e1
bryano13 <bryano13@hotmail.com>
2023-11-10 15:16:32 -0500

    trying

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - a program to cut a string in half
+ * a simple program to print a string in half
+ * @str: a string
+ * Return a halfed string
+ */
+void puts_half(char *str)
+{
+	int c, i;
+
+	i = c 
+
+	for (c = 0; *str != '\0' ; str++)
+		c++;
+
+	if (c / 2 == 0)
+	{
+
+		_putchar(*str / 2);
+	}
+
+	else
+		_putchar(*str - 1 / 2 % 10);
+
+	_putchar('\n');
+}
