ac6f9cca790009efa3bc51e1b32acc5c6d0db339
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-14 16:13:54 -0500

    first try

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - 2th func
+ * print all the letters of the alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, c;
+
+	i = 0;
+
+	while (i < 11)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+
+		i++;
+		_putchar('\n');
+	}
+}
