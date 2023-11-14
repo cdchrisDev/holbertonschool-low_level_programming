17524b83838c25a0c13bbe7809a3bdde9f851ba1
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-14 16:00:30 -0500

    done

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - second called
+ * a program to print alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
