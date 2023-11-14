5def9e2ece5840c583edb6b92dee68db50d307ad
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-14 17:10:15 -0500

    done

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - 2th func called
+ * a func to check if its positive and negative and just print the sign
+ * @n: a number
+ * Return: a sign
+ */
+int print_sign(int n)
+{
+	int pos, neg;
+
+	pos = 43;
+	neg = 45;
+
+	if (n > 0)
+	{
+		_putchar(pos);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(neg);
+		return (-1);
+	}
+	else
+		_putchar('0');
+		return (0);
+}
