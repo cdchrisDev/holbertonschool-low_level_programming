5dca32a255a67a7a2a92138faf6eeba3819e3583
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-15 15:35:58 -0500

    done

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - 2th called to func
+ * a func to multiply two num
+ * @a: den
+ * @b: mul
+ * Return: r = a * b;
+ */
+int mul(int a, int n);
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}
