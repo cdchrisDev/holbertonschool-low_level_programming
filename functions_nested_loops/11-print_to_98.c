39ecba1285631d1f8067ce5def69edb510daec7d
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-15 14:16:09 -0500

    hols

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - 2th func called
+ * a func to print natural numbers
+ * @n: a int
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+
+}
