d36d1963a9fac954af09b5145454d715056a1d45
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-16 10:22:39 -0500

    done

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,18 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
+		{
+			if (i == 15)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
 
 		else if (i % 5 == 0)
 		{
 			if (i == 100)
 				printf("Buzz");
+
 			else
 				printf("Buzz ");
 		}
