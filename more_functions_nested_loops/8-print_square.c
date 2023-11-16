af0c230aef46732303e4b9377c6bb299c192385e
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-16 08:47:33 -0500

    done

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,17 @@
  */
 void print_square(int size)
 {
-	int i, j;	
+	int i, j;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			_putchar('#');
+			if (size <= 0)
+				_putchar('\n');
+
+			else
+				_putchar('#');
 		}
 		_putchar('\n');
 	}
