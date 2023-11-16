abab0e285ed1b926f84de497f703a9c42bf2a087
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-16 10:11:14 -0500

    done

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * a program to print FizzBuzz depending its multiply
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+
+	}
+
+	return (0);
+}
