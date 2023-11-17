1ae50d0a7c23619335d3b8c72225df31e00e5029
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-16 22:47:48 -0500

    done

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcpy - a func to print copy a string
+ * @dest: where it is copied
+ * @src: where it copys
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while ((*dest++ = *src++) != '\0')
+		;
+
+	return (dest);
+}
