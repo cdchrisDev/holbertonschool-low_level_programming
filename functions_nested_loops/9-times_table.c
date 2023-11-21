e7366b94577bb75a29a62f19893e2dc0ef06f28e
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-21 16:20:29 -0500

    done

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,70 +1,41 @@
 #include "main.h"
+/**
+ * times_table - a function to print 9th table
+ * @void: no parameters
+ * Return: no return
+ */
 
 void times_table(void)
 {
-    int iz, der, den, mul, result;
-    int i = 0;
+	int den, mul, result;
 
-    for (den = 0; den != 10; den++)
-    {
-        mul = 0;
-        
-        /*print the 0 * 9 table*/
-        while (i != 10)
-        {
-          result = den * mul;
+	for (den = 0; den < 10; den++)
+	{
 
-		iz = (result / 10) + '0';
-    		der = (result % 10) + '0';
-
-            if (result == 0 && i == 0 && den == 0)
-            {
-                _putchar(result + '0');
-                _putchar(',');
-                _putchar(' ');
-		_putchar(' ');
-            }
-            else if (i == 9)
-            {
-                if (result < 10)
-                {
-                    _putchar(result + '0');
-                    _putchar('\n');
-                }
-                else
-                {
-                    _putchar(iz);
-                    _putchar(der);
-                    _putchar('\n');
-                }
-            }
-            else
-            {
-                if (result < 10)
-                {
-                    _putchar(result + '0');
-                }
-                else
-                {
-                    _putchar(iz);
-                    _putchar(der);
-                }
-		if (den * mul + den > 9)
+		for (mul = 0; mul < 10; mul++)
 		{
-                	_putchar(',');
-                	_putchar(' ');
+			result = den * mul;
+			/* result */
+			if (result >= 0 && result <= 9)
+				_putchar(result + '0');
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			/* identation */
+			if (result + den >= 10 && mul != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (result + den <= 10 && mul != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
 		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
-                if (den != 0)
-                    mul++;
-            }
-            i++;
-        }
-        i = 0;
-     }
+		_putchar('\n');
+	}
 }
