f16636b273241ca71b1f92f60c4c7b74b1a8abd5
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-24 04:31:30 -0800

    done

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,8 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+int _atoi(char *s);
+char *_strncpy(char *dest, char *src, int n);
+
 
 #endif
