6aa2b284b349c4e200cb542c307d244bb5ebbd60
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-25 15:22:07 -0800

    done

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - a function to encode into 1337
+ * @c: char
+ * Return: string encoded
+ */
+char *leet(char *c)
+{
+	int i;
+/* Minus */
+	while (c[i++] != '\0')
+	{
+		if (c[i] == 'a' || c[i] == 'A')
+			c[i] = '4';
+		else if (c[i] == 'e' || c[i] == 'E')
+			c[i] = '3';
+		else if (c[i] == 'o' || c[i] == 'O')
+			c[i] = '0';
+		else if (c[i] == 't' || c[i] == 'T')
+			c[i] = '7';
+		else if (c[i] == 'l' || c[i] == 'L')
+			c[i] = '1';
+	}
+	return (c);
+}
