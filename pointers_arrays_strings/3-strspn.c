8912b40bd2d242a0048517022356a910c1370b64
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-26 06:47:32 -0800

    not me

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,6 +10,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i = 0, j = 0;
 
 	while (s[i++] != '\0')
+	{
 		while (accept[j++] != '\0')
 		{
 			if (s[i] == accept[j])
@@ -18,5 +19,6 @@ unsigned int _strspn(char *s, char *accept)
 			if (!(accept[j]))
 				break;
 		}
+	}
 	return (i);
 }
