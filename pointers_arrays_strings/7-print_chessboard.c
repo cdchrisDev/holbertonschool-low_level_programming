cae488cc23e0ce15ece9c0d4002112e83af81034
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-28 12:34:14 -0800

    done

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+
+	for (; i < 8; i++)
+	{
+		int j = 0;
+
+		for (; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
