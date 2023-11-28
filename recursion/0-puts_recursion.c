7d4dae58aaa869e53222c0b9f114bf78d499360e
cdchrisDev <juan.feli.lopez@gmail.com>
2023-11-28 13:46:42 -0800

    done

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - a func to print a string, followed newline
+ * @s: a char
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+		_puts_recursion(s);
+	else
+		_putchar('\n');
+}
