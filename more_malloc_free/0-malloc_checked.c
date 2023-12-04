54fbdfc25ed87d98c69c3826e000b5906c8814c2
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-03 19:07:03 -0800

    first try

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - func that allocates memory using malloc
+ * @b: memory to alloc
+ * Return: a pointer to the alloc mem
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *B = malloc(b);
+
+	return (B);
+}
