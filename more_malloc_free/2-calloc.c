75649b34876d56f24b8265eb519e98f60f8931b7
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-04 07:49:15 -0800

    4th try

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	i = malloc(sizeof(unsigned int) * nmemb);
+	i = malloc(sizeof(unsigned int) * nmemb / size);
 
 	while (count < nmemb)
 	{
