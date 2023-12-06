f1e516df7584b74a9a8282cfc6fa89a1d44380a1
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-06 15:37:48 -0800

    first try

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,10 +11,10 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		putchar(' ');
+		d = NULL;
 
 	if (d->name == NULL)
-		;
+		d->name = NULL;
 
 	printf("Name: %s\nAge: %.7f\nOwner: %s\n", d->name, d->age, d->owner);
 }
