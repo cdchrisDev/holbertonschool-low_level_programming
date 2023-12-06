26db4e806f7c034b5533b6cd245bc4cf3ce676c0
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-06 11:01:05 -0800

    done

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - print a text
+ * @name: dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: text
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
