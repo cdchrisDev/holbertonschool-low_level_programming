0638d7dda707bdc1931a7a7244d4331cf22d8468
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-06 19:52:36 -0800

    4th try

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -26,14 +26,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/*new location for strings*/
 	new__dog->name = malloc(i + 1);
 	new__dog->owner = malloc(j + 1);
-
-	if (new__dog->name == NULL || new__dog->owner == NULL)
-	{
-		free(new__dog->name);
-		free(new__dog->owner);
-		free(new__dog);
-		return (NULL);
-	} /* count in reverse and asign strings */
+	
+	/* count in reverse and asign strings */
 	while (i > I)
 	{
 		new__dog->name[I] = name[I];
@@ -45,5 +39,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		new__dog->owner[J] = owner[J];
 		J++;
 	}
+	free(new__dog->name);
+	free(new__dog->owner);
+	free(new__dog);
 	return (new__dog);
 }
