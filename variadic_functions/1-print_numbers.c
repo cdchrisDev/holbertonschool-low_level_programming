a6c8fc2f970304ab3b7ec90e9aad9c86ae33e1a5
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-10 22:27:46 -0800

    done

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,10 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-			return;
-
 		if (1 + i == n)
+			printf("%d", va_arg(MoArg, int));
+		else if (separator == NULL)
 			printf("%d", va_arg(MoArg, int));
 		else
 			printf("%d%s", va_arg(MoArg, int), separator);
