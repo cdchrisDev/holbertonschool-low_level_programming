d34423736856f5ee0c2c6740bebb7f18eece3448
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-15 17:01:44 -0800

    dda

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,14 +11,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int k, i = 0, j = 0;
-	char *NAME = &name[i], *OWNER = &owner[j];
 	/*new struct of type dog_t*/
 	dog_t *new__dog;
-
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-
-	new__dog = malloc(sizeof(dog_t));
+	new__dog = (dog_t *)malloc(sizeof(dog_t));
 
 	if (new__dog == NULL)
 		return (NULL);
@@ -29,22 +24,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 	while (owner[j] != '\0')
 		j++;
 
-	new__dog->name = malloc(sizeof(char) * i);
-	new__dog->owner = malloc(sizeof(char) * j);
+	new__dog->name = malloc(i + 1);
+	new__dog->owner = malloc(j + 1);
 
 	if (new__dog->name == NULL || new__dog->owner == NULL)
 	{
 		free(new__dog->name);
 		free(new__dog->owner);
+
 		free(new__dog);
-
 		return (NULL);
-	}
-	for (k = 0; k <= i; k++)
-		new__dog->name[k] = NAME[k];
-	for (k = 0; k <= j; k++)
-		new__dog->owner[k] = OWNER[k];
+	}	
 
+	for (k = 0; k <= i; k++)
+		new__dog->name[k] = name[k];
+	for (k = 0; k <= j; k++)
+		new__dog->owner[k] = owner[k];
+
+	new__dog->name[i] = '\0';
+	new__dog->owner[j] = '\0';
 	new__dog->age = age;
 
 	return (new__dog);
