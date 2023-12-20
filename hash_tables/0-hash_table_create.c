ba97a23f7e5ff90afbf14c36ed40f511a3a838ca
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-19 22:58:10 -0800

    done

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,22 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * hash_table_t - a func that creates a hash table. 
+ * hash_table_create - a func that creates a hash table.
  * @size: is the size of the array
  * Return: pointer to created hash table, NULL if goes wrong
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-
-	ht = malloc(sizeof(*ht));
-
-	ht->size = size;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
 
-	ht->array = calloc(sizeof(hash_table_t*), ht->size);
+	ht->size = size;
+
+	ht->array = calloc(sizeof(foo *), ht->size);
 	/* Calloc init in 0 */
-	return ht; 
+	return (ht);
 }
