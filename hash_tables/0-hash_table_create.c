e7e5822dd30ce117c3986867a8de55298ac53b3b
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-19 23:02:09 -0800

    done

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,14 +9,18 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
+	ht->array = malloc(sizeof(hash_table_t) * ht->size);
 
-	ht->array = calloc(sizeof(foo *), ht->size);
+	for (i = 0; i < size; i++)
+		if (ht->array[i] == NULL)
+			return (NULL);
 	/* Calloc init in 0 */
 	return (ht);
 }
