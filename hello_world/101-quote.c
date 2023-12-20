9232ac1b91df70cab38d276207d5541950f668e1
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-19 19:01:58 -0800

    done

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 1
+ */
+int main(void)
+{
+	char *c = " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	while (*c++)
+		putchar(*c);
+
+	return (1);
+}
