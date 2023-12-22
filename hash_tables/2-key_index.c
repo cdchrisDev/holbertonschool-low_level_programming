ab1dfa37b0b2245be9c79838bc21f31ef75250ca
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-22 08:27:13 -0800

    done

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,24 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
- */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
-}
-/**
  * key_index - a func that gives you the index of a key 
  * @key: the key
  * @size: the size of the array
