fccf9c2bc3061fa9c335de8e6a099494675205ab
cdchrisDev <juan.feli.lopez@gmail.com>
2024-01-13 15:51:44 -0800

    second try

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * binary_to_uint - a func to convert from binary to uint
- * @b: string of 0 or 1 char
- * Return: uint
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
-	int base = 1, i;
+	int base = 1, i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i + 1] != '\0'; i++)
+	while (b[i + 1])
+	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		i++;
+	}
 
 	while (i >= 0)
 	{
@@ -22,5 +25,8 @@ unsigned int binary_to_uint(const char *b)
 		base *= 2;
 		i--;
 	}
+
+
 	return (res);
+
 }
