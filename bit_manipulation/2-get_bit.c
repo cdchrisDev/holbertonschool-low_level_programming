bd18cd0058becf2f4ec775dafecef63d6c3395e7
cdchrisDev <juan.feli.lopez@gmail.com>
2024-01-13 16:48:49 -0800

    done

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - a func to return value of a bit on index
+ * @n: integer
+ * @index: the index
+ * return: integer
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (sizeof(n) * 8 < index)
+		return (-1);
+
+	n >>= index;
+	if ((n & 1) == 1)
+		return (1);
+	else
+		return (0);
+}
