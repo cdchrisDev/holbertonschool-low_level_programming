b66b4beb8eb73530c19dbcc6d7bd5577e2936768
cdchrisDev <juan.feli.lopez@gmail.com>
2024-01-14 16:34:47 -0800

    freed items fixed

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -25,6 +25,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			{
 				free(s->value);
 				s->value = strdup(value);
+				if (s->value == NULL)
+					return (0);
+
 				return (1);
 			}
 
